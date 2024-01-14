f7f7b0406e561fcec171538d9849a9613f8f344a
JustinTrotter <trotter.justin@gmail.com>
2024-01-13 23:45:39 -0600

    shoots based on chord timing

--- bar.gd
+++ bar.gd
@@ -1,24 +1,31 @@
 extends Node2D
 
-@onready var start_position = %"Start Position"
-@onready var target_position = %"Target Position"
+@onready var chord_1= %"Start Position"
+@onready var chord_2 = %chord_2
+@onready var chord_3 = %chord_3
+@onready var chord_4 = %chord_4
+@onready var chord_5 = %"Target Position"
+
+@onready var note_number = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_move_bar(chord_2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		_move_bar()
+	pass
 
-func _move_bar():
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "global_position", target_position.global_position, 1.0)
-	tween.tween_callback(_on_tween_complete)
-
-func _on_tween_complete():
-	self.global_position = start_position.global_position
+func _move_bar(target):
 	Globals.tempo_tick.emit()
-	_move_bar()
+	note_number += 1
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", target.global_position, 0.5)
+	tween.tween_callback(_on_tween_complete)
+	
+func _on_tween_complete():
+	if note_number == 6:
+		self.global_position = chord_1.global_position
+		note_number = 2
+	_move_bar(get("chord_" + str(note_number)))

c1f1ba9ac7c34ea0544c3b0a5d1053db60a4d7af
Justin Trotter <trotter.justin@gmail.com>
2024-01-21 13:18:41 -0600

    flying component works

--- a_star.gd
+++ a_star.gd
@@ -3,15 +3,18 @@ extends Node2D
 @onready var map = get_parent()
 @onready var camera_target = get_tree().get_nodes_in_group("CameraTarget")[0]  
 @onready var astar_grid = AStarGrid2D.new()
+@onready var astar_fly_grid = AStarGrid2D.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Globals.connect("changed_level", on_changed_level)
+	setup_grid(astar_grid)
+	setup_grid(astar_fly_grid)
 	
-	astar_grid.region = Rect2i(0, 0, Globals.level_screen_tile_width, Globals.level_screen_tile_height)
-	astar_grid.cell_size = Vector2(1, 1)
-	#astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
-	astar_grid.update()
+func setup_grid(grid: AStarGrid2D):
+	grid.region = Rect2i(0, 0, Globals.level_screen_tile_width, Globals.level_screen_tile_height)
+	grid.cell_size = Vector2(1, 1)
+	grid.update()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -48,9 +51,10 @@ func level_position_from_tile_position(tile_position: Vector2) -> Vector2:
 	return result
 
 func on_changed_level():
-	update_astar_grid_with_wall_data()
+	update_astar_grid_with_wall_data(astar_grid, ["Walls", "Water"])
+	update_astar_grid_with_wall_data(astar_fly_grid, ["Walls"])
 	
-func update_astar_grid_with_wall_data():
+func update_astar_grid_with_wall_data(grid: AStarGrid2D, collision_names):
 	var non_null_levels = map.level_lookup_map.filter(func (level): 
 		return level != null)
 	var target_level_node = non_null_levels.filter(func (level): 
@@ -58,5 +62,9 @@ func update_astar_grid_with_wall_data():
 	var target_level_map = target_level_node.get_children()[0]
 	for i in range(0, Globals.level_screen_tile_width):
 		for j in range(0, Globals.level_screen_tile_height):
+			grid.set_point_solid(Vector2i(i, j), false)
 			var target_tile_data = target_level_map.get_cell_tile_data(0, Vector2(i,j)).get_custom_data_by_layer_id(0)
-			astar_grid.set_point_solid(Vector2i(i, j), target_tile_data != {})
+			for name in collision_names:
+				if target_tile_data != {}:
+					if target_tile_data["enums"][0] == name:
+						grid.set_point_solid(Vector2i(i, j), true)
--- bundles/game/path_to_player.gd
+++ bundles/game/path_to_player.gd
@@ -3,6 +3,21 @@ extends Node2D
 @onready var player = get_tree().get_nodes_in_group("Player")[0]  
 @onready var a_star = get_tree().get_nodes_in_group("A_Star")[0]
 
+@onready var a_star_wall_grid = a_star.astar_grid
+@onready var a_star_fly_grid = a_star.astar_fly_grid
+
+@onready var a_star_current_grid = a_star_wall_grid
+
+func _ready():
+	Globals.connect("changed_level", on_changed_level)
+		
+func on_changed_level():
+	if get_parent().get_parent().has_node("Flying"):
+		a_star_current_grid = a_star.astar_fly_grid
+	else:
+		a_star_current_grid = a_star.astar_grid
+	
+
 func get_relative_tile_position(tile_position: Vector2, level_position: Vector2) -> Vector2:
 	var result = Vector2(
 		tile_position.x - (Globals.level_screen_tile_width * level_position.x), 
@@ -17,15 +32,16 @@ func undo_relative_tile_position(relative_tile_position: Vector2, level_position
 
 
 func _physics_process(delta):
-	var level_position = a_star.get_grid_coords_from_position(get_parent().get_parent().position)
-	var tile_position = a_star.get_tile_coords_from_position(get_parent().get_parent().position)
-	var relative_tile_position = get_relative_tile_position(tile_position, level_position)
-	var player_level_position = a_star.get_grid_coords_from_position(player.position)
-	var player_tile_position = a_star.get_tile_coords_from_position(player.position)
-	var player_relative_tile_position = get_relative_tile_position(player_tile_position, player_level_position)
-	var path = a_star.astar_grid.get_id_path(relative_tile_position, player_relative_tile_position)
-	if path.size() <= 1: return
-	var undo_relative_target_position = undo_relative_tile_position(path[1], level_position)
-	var undo_target_tile_coords_from_position = a_star.get_position_from_tile_coords(undo_relative_target_position)
-	get_parent().direction = ((undo_target_tile_coords_from_position + Vector2(8, 8)) - get_parent().get_parent().position).normalized()
+	if a_star_current_grid != null:
+		var level_position = a_star.get_grid_coords_from_position(get_parent().get_parent().position)
+		var tile_position = a_star.get_tile_coords_from_position(get_parent().get_parent().position)
+		var relative_tile_position = get_relative_tile_position(tile_position, level_position)
+		var player_level_position = a_star.get_grid_coords_from_position(player.position)
+		var player_tile_position = a_star.get_tile_coords_from_position(player.position)
+		var player_relative_tile_position = get_relative_tile_position(player_tile_position, player_level_position)
+		var path = a_star_current_grid.get_id_path(relative_tile_position, player_relative_tile_position)
+		if path.size() <= 1: return
+		var undo_relative_target_position = undo_relative_tile_position(path[1], level_position)
+		var undo_target_tile_coords_from_position = a_star.get_position_from_tile_coords(undo_relative_target_position)
+		get_parent().direction = ((undo_target_tile_coords_from_position + Vector2(8, 8)) - get_parent().get_parent().position).normalized()
 
